faa25079d675e364cfcf137044d0736ad85abe1f
Asharkhan468 <asharullah.khan2468@gmail.com>
2024-03-11 21:07:12 +0500

    Array Javascript

---
+++ app.js
@@ -0,0 +1,59 @@
+//Array Chap no 5
+
+
+//Question no 1
+
+let studentName=[``];
+
+
+//Question no 2
+
+
+
+
+//Question no 3
+
+let number=[12 , 45 , 78 , 90];
+console.log(number);
+
+
+
+//Question no 4
+
+let string=['Ashar' , 'Ibrahim' , 'Raza' ,'Rafy'];
+console.log(string);
+
+
+
+//Question no 5
+
+let boolean=[true , false , false , true];
+console.log(boolean);
+
+
+//Question no 6
+
+let items=['Desk' , true , 34];
+
+console.log(items);
+
+
+
+//Question no 7
+
+let mobileNetworks=['Zong' , 'Ufone' , 'Jazz' ,'Telenor' ];
+console.log(mobileNetworks);
+
+
+
+//Question no 8
+
+let education=[ 'SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS',
+'M. Phil', 'PhD'];
+
+document.write(education)
+
+
+//Question no 9
+
+let moviesName=[]
