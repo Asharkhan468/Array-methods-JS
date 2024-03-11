b5bc6db2edc72f21bbbd71f4941b69853a09b208
Asharkhan468 <asharullah.khan2468@gmail.com>
2024-03-12 00:06:49 +0500

    Array js

--- app.js
+++ app.js
@@ -1,63 +1,85 @@
 //Array Chap no 5
 
-
 //Question no 1
 
-let studentName=[``];
-
+let studentName = [``];
 
 //Question no 2
 
-
-
-
 //Question no 3
 
-let number=[12 , 45 , 78 , 90];
+let number = [12, 45, 78, 90];
 console.log(number);
-
-
 
 //Question no 4
 
-let string=['Ashar' , 'Ibrahim' , 'Raza' ,'Rafy'];
+let string = ["Ashar", "Ibrahim", "Raza", "Rafy"];
 console.log(string);
-
-
 
 //Question no 5
 
-let boolean=[true , false , false , true];
+let boolean = [true, false, false, true];
 console.log(boolean);
-
 
 //Question no 6
 
-let items=['Desk' , true , 34];
+let items = ["Desk", true, 34];
 
 console.log(items);
 
-
-
 //Question no 7
 
-let mobileNetworks=['Zong' , 'Ufone' , 'Jazz' ,'Telenor' ];
+let mobileNetworks = ["Zong", "Ufone", "Jazz", "Telenor"];
 console.log(mobileNetworks);
-
-
 
 //Question no 8
 
-let education=[ 'SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS',
-'M. Phil', 'PhD'];
+let education = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD"];
 
-document.write(education)
-
+document.write(education);
 
 //Question no 9
 
-let moviesName=[input.value];
+// let input=document.querySelector('.input-field');
 
-let input=document.querySelector('.input-field');
+// function myFunct() {
 
-console.log(moviesName);
+//     let inputfield=input.value
+//     let greek=[]
+
+//     for(i=0 ; i<=inputfield ; i++){
+
+//         greek[i]=input.value;
+//         document.write(greek[i]+ '<br>')
+//     }
+
+// }
+
+
+
+//Question no 10
+
+
+
+let cars = ["Audi", "Ferarri", "Vigo", "Fortuner"];
+
+console.log(`First index  of Array ` + cars.indexOf("Audi"));
+console.log(`Car at first index of Array ` + cars[0]);
+
+console.log(`last index of the Array ` + cars.indexOf("Fortuner"));
+console.log(`Car at last index of Array ` + cars[3]);
+
+
+
+
+//Question no 11
+
+
+let totalScore=500;
+let student=['Ashar' ,'Bilal' ,'Hamza'];
+let obtainedScore=[488 , 345 , 443];
+
+
+console.log('The  score of michale');
+console.log('The  score of Jonny ');
+console.log('The  score of Tony ');
